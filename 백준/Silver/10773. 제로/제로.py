77603cde6c3a927bfad91b03917f299bb8ccf85d
elena <165857023+HyelenaZ@users.noreply.github.com>
2025-01-19 23:57:46 +0900

    [Silver IV] Title: 제로, Time: 56 ms, Memory: 33192 KB -BaekjoonHub

--- 백준/Silver/10773. 제로/제로.py
+++ 백준/Silver/10773. 제로/제로.py
@@ -1,21 +1,24 @@
-def cal_fin_sum():
-    try: 
-        K = int(input())
-        if not (1<= K <= 100000):
-            print("K는 1~100,000 사이의 정수")
-            return 0
+import sys
+input = sys.stdin.readline
 
+def cal_fin_sum():
+    try:
+        K = int(input())
+        if not (1 <= K <= 100000):
+            return 0
+            
         stack = []
         for _ in range(K):
-            num = int(input().strip())
+            num = int(input())
             if num == 0 and stack:
-                removed = stack.pop()
+                stack.pop()
             elif num > 0:
                 stack.append(num)
+                
         return sum(stack)
-
+    
     except ValueError:
         return 0
 
-result= cal_fin_sum()
+result = cal_fin_sum()
 print(result)
