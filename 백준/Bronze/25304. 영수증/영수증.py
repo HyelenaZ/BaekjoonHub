0b683d4ee07bc5c70166bcf0f2b2e5ec6037d125
elena <165857023+HyelenaZ@users.noreply.github.com>
2025-01-21 01:36:55 +0900

    [Bronze IV] Title: 영수증, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/25304. 영수증/영수증.py
@@ -0,0 +1,12 @@
+import sys
+input = sys.stdin.readline
+
+X = int(input())
+N = int(input())
+total = 0 
+
+for _ in range(N):
+    a, b = map(int, input().split())
+    total += a * b
+
+print("Yes" if X == total else "No")
