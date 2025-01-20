576a8c60eba53998e0aadf6b0579e328d234ad58
elena <165857023+HyelenaZ@users.noreply.github.com>
2025-01-21 02:01:40 +0900

    [Bronze IV] Title: 영수증, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/25304. 영수증/영수증.py
+++ 백준/Bronze/25304. 영수증/영수증.py
@@ -1,12 +1,22 @@
 import sys
 input = sys.stdin.readline
 
-X = int(input())
-N = int(input())
-total = 0 
+def check_receipt():
+    X = int(input())
+    if not (1 <= X <= 1_000_000_000):
+        return "No"
+    
+    N = int(input())
+    if not (1 <= N <= 100):
+        return "No"
+    
+    total = 0
+    for _ in range(N):
+        a, b = map(int, input().split())
+        if not (1 <= a <= 1_000_000 and 1 <= b <= 10):
+            return "No"
+        total += a*b
+    
+    return "Yes" if X == total else "No"
 
-for _ in range(N):
-    a, b = map(int, input().split())
-    total += a * b
-
-print("Yes" if X == total else "No")
+print(check_receipt())
