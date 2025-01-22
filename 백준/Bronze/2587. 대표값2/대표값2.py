d431a50d4661387959cb8b6ff81ebad26c2cb6d4
elena <165857023+HyelenaZ@users.noreply.github.com>
2025-01-23 02:30:23 +0900

    [Bronze II] Title: 대표값2, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/2587. 대표값2/대표값2.py
+++ 백준/Bronze/2587. 대표값2/대표값2.py
@@ -1,12 +1,4 @@
-##### 240115 : CALCULATE mean & median
-number_lst = [int(input()) for _ in range(5)]
-number_lst.sort()
-
-def calculate_stats(numbers):
-    mean = sum(numbers)/len(numbers)
-    median = sorted(numbers)[len(numbers)//2]
-    return mean, median
-
-mean, median = calculate_stats(number_lst)
-print(int(mean))
-print(median)
+numbers = sorted([int(input()) for _ in range(5)])
+total = sum(numbers)
+print(total // 5)
+print(numbers[2])
