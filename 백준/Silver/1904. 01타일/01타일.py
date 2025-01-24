5ab0f20f150b1734ca6bf5fe4afddfbdf5233e7c
elena <165857023+HyelenaZ@users.noreply.github.com>
2025-01-25 04:24:40 +0900

    [Silver III] Title: 01타일, Time: 120 ms, Memory: 32544 KB -BaekjoonHub

--- 백준/Silver/1904. 01타일/01타일.py
+++ 백준/Silver/1904. 01타일/01타일.py
@@ -1,16 +1,11 @@
 def tile(n: int) -> int:
-    MOD = 15746
-    if n == 1:
-        return 1
-    elif n == 2:
-        return 2
-    
+    MOD = 15746    
     a, b = 1, 2
     
     for _ in range(3, n + 1):
         a, b = b, (a + b) % MOD
     
-    return b
+    return a if n == 1 else b
 
 n = int(input())
 print(tile(n))
