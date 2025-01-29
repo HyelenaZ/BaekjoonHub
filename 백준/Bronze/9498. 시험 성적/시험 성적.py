757db614941f1569fa9e33707437679049cd4fb2
elena <165857023+HyelenaZ@users.noreply.github.com>
2025-01-30 04:15:03 +0900

    [Bronze V] Title: 시험 성적, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/9498. 시험 성적/시험 성적.py
+++ 백준/Bronze/9498. 시험 성적/시험 성적.py
@@ -1,11 +1,5 @@
+import sys
+input = sys.stdin.readline
 score = int(input())
-if 90<=score<=200:
-    print("A")
-elif 80<=score<=89:
-    print("B")
-elif 70<=score<=79:
-    print("C")
-elif 60<=score<=69:
-    print("D")
-else:
-    print("F")
+grade = 'A' if score >= 90 else 'B' if score >= 80 else 'C' if score >=70 else 'D' if score >= 60 else 'F'
+print(grade)
