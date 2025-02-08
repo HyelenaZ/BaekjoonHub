ff363bd12c5d75f2abd58f35f94138b062625982
elena <165857023+HyelenaZ@users.noreply.github.com>
2025-02-08 21:46:15 +0900

    [Silver II] Title: 자원 캐기, Time: 76 ms, Memory: 34852 KB -BaekjoonHub

--- 백준/Silver/14430. 자원 캐기/자원 캐기.py
+++ 백준/Silver/14430. 자원 캐기/자원 캐기.py
@@ -1,20 +1,22 @@
-N, M = map(int, input().split())
-grid = []
-for _ in range(N):
-    row = list(map(int, input().split()))
-    grid.append(row)
+import sys
+input = sys.stdin.readline
 
-# DP 배열 (자원의 최대값 저장)
+N, M = map(int, input().split())
+grid = [list(map(int, input().split())) for _ in range(N)]
+
+# DP 배열 (자원의 최대 개수 저장)
 dp = [[0] * M for _ in range(N)]
 dp[0][0] = grid[0][0]
 
-# 첫 행 
+# 첫 행 초기화
 for j in range(1, M):
     dp[0][j] = dp[0][j-1] + grid[0][j]
-# 첫 열
+
+# 첫 열 초기화
 for i in range(1, N):
     dp[i][0] = dp[i-1][0] + grid[i][0]
-# 나머지 칸 계산
+
+# 나머지 칸 채우기
 for i in range(1, N):
     for j in range(1, M):
         dp[i][j] = max(dp[i-1][j], dp[i][j-1]) + grid[i][j]
