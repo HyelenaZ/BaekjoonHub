8076eb66daf2925aeb6fcb1259eaa91d764b3095
elena <165857023+HyelenaZ@users.noreply.github.com>
2025-02-10 00:05:53 +0900

    [Bronze V] Title: 개수 세기, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/10807. 개수 세기/개수 세기.py
+++ 백준/Bronze/10807. 개수 세기/개수 세기.py
@@ -2,7 +2,7 @@ import sys
 input = sys.stdin.readline
 
 N = int(input())
-num = list(map(int, input().split()))
+nums = list(map(int, input().split()))
 v = int(input())
 
-print(num.count(v))
+print(len([num for num in nums if num == v]))
