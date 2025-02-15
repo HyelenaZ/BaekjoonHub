4cd1a05f411f223bae5b3ae06671fea4b001f620
elena <165857023+HyelenaZ@users.noreply.github.com>
2025-02-15 23:30:55 +0900

    [Silver IV] Title: 어두운 굴다리, Time: 88 ms, Memory: 42660 KB -BaekjoonHub

---
+++ 백준/Silver/17266. 어두운 굴다리/어두운 굴다리.py
@@ -0,0 +1,34 @@
+N = int(input())
+M = int(input())
+lamp_positions = list(map(int, input().split()))
+
+height = 1
+while height <= N:
+    if lamp_positions[0] - height > 0:
+        height += 1
+        continue
+        
+    if lamp_positions[-1] + height < N:
+        height += 1
+        continue
+        
+    has_dark_spot = False
+    
+    for i in range(M-1):
+        now_lamp = lamp_positions[i]
+        next_lamp = lamp_positions[i+1]
+        
+        now_end = now_lamp + height
+        next_start = next_lamp - height
+        
+        if now_end < next_start:
+            has_dark_spot = True
+            break
+            
+    if has_dark_spot:
+        height += 1
+        continue
+    
+    break
+
+print(height)
