0b3d0645897feac7fcd62a73b8f78fee46ecfc7b
elena <165857023+HyelenaZ@users.noreply.github.com>
2025-02-17 22:27:14 +0900

    [Silver II] Title: 나무 자르기, Time: 2020 ms, Memory: 147648 KB -BaekjoonHub

--- 백준/Silver/2805. 나무 자르기/나무 자르기.py
+++ 백준/Silver/2805. 나무 자르기/나무 자르기.py
@@ -1,26 +1,24 @@
 import sys
+from bisect import bisect_left
+
 input = sys.stdin.readline
 N, M = map(int, input().rstrip().split())
-trees = list(map(int, input().rstrip().split()))
+trees = sorted(list(map(int, input().rstrip().split())))
 
-start = 0
-end = max(trees)
+def get_tree_amount(height):
+    idx = bisect_left(trees, height)
+    return sum(tree - height for tree in trees[idx:])
+
+start, end = 0, trees[-1]
 H = 0
 
 while start <= end:
-    current_H = (start + end) //2
+    current_H = (start + end) // 2
+    amount = get_tree_amount(current_H)
     
-    get_tree = 0 
-    for tree in trees:
-        if tree > current_H:
-            get_tree += tree - current_H
-            if get_tree >= M:
-                break
-    
-    if get_tree >= M:
+    if amount >= M:
         H = current_H
-        start = current_H + 1 
+        start = current_H + 1
     else:
-        end = current_H -1
-
+        end = current_H - 1
 print(H)
