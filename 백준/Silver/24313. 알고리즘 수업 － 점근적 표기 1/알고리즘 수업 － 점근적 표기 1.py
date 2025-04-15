9395359714dc871883a010b5c10157c91e2e943c
elena <165857023+HyelenaZ@users.noreply.github.com>
2025-04-15 23:47:35 +0900

    [Silver V] Title: 알고리즘 수업 - 점근적 표기 1, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/24313. 알고리즘 수업 － 점근적 표기 1/알고리즘 수업 － 점근적 표기 1.py
@@ -0,0 +1,14 @@
+import sys
+input = sys.stdin.readline
+
+def check_O(a1, a0, c, n0):
+    for n in range(n0, 101):  
+        if a1 * n + a0 > c * n:
+            return 0 
+    return 1  
+
+a1, a0 = map(int, input().split()) 
+c = int(input())  
+n0 = int(input()) 
+
+print(check_O(a1, a0, c, n0))
